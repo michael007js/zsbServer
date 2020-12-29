db9f8c99fa6bcad4eed742696b534e3d4322bae3
michael007js <616425434@qq.com>
2020-12-29 20:46:21 +0800

    更新一键调用逻辑

--- src/sample/module/LeiDianModule.java
+++ src/sample/module/LeiDianModule.java
@@ -1,5 +1,11 @@
 package sample.module;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -12,6 +18,7 @@ import sample.module.adapter.LdSimulatorAdapter;
 import utils.*;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("ALL")
 public class LeiDianModule extends BaseTabModule implements EventHandler<ActionEvent> {
@@ -122,37 +129,77 @@ public class LeiDianModule extends BaseTabModule implements EventHandler<ActionE
     }
 
     private void autoCreateLaunchInstallRunApk(boolean deteleAll) {
-       new Thread(){
-           @Override
-           public void run() {
-               super.run();
-               ArrayList<LeiDianSimulatorBean> list = LeiDian.getInstance().getSimulatorList();
-               LeiDian.getInstance().quit(-1);
-               for (int i = 0; i < list.size(); i++) {
-                   if (deteleAll || list.get(i).getName().equals(controller.getEdit_ld_name().getText())) {
-                       LeiDian.getInstance().removeByIndex(list.get(i).getPosition());
-                   }
-               }
-               LeiDian.getInstance().add(controller.getEdit_ld_name().getText());
-               getSimulatorList();
-               LeiDian.getInstance().modifyDisplay(1, 720, 1080, 240);
-               MobileBrand mobileBrand = MobileBrandUtils.getRandomMobileBrand();
-               LeiDian.getInstance().modifyManufacturer(1, mobileBrand.getBrand());
-               LeiDian.getInstance().modifyModel(1, mobileBrand.getTitle());
-               LeiDian.getInstance().modifyPhoneNumber(1, RandomPhoneNumber.createMobile(1));
-               LeiDian.getInstance().modifyImsi(1, true, 0);
-               LeiDian.getInstance().modifyImei(1, true, 0);
-               LeiDian.getInstance().modifySimSerial(1, true, 0);
-               LeiDian.getInstance().modifyAndroidId(1, true, 0);
-               LeiDian.getInstance().modifyMac(1, true, "");
-               LeiDian.getInstance().installApp(1, "C:/Users/Administrator/Desktop/d0f205e61a15ed8c5658c5c102107b5c_40578.apk");
-               try {
-                   sleep(20000);
-                   LeiDian.getInstance().runApp(1,"com.supercell.clashofclans.guopan");
-               } catch (InterruptedException e) {
-                   e.printStackTrace();
-               }
-           }
-       }.start();
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                LogUtils.e("退出所有模拟器");
+                emitter.onNext(LeiDian.getInstance().quit(-1));
+            }
+        }).map(new Function<String, ArrayList<Integer>>() {
+            @Override
+            public ArrayList<Integer> apply(String s) throws Exception {
+                ArrayList<Integer> list = new ArrayList<>();
+                ArrayList<LeiDianSimulatorBean> simulator = LeiDian.getInstance().getSimulatorList();
+                for (int i = 0; i < simulator.size(); i++) {
+                    if (deteleAll || simulator.get(i).getName().equals(controller.getEdit_ld_name().getText())) {
+                        list.add(simulator.get(i).getPosition());
+                    }
+                }
+                LogUtils.e("统计符合条件的待移除模拟器");
+                return list;
+            }
+        }).map(new Function<ArrayList<Integer>, String>() {
+            @Override
+            public String apply(ArrayList<Integer> integers) throws Exception {
+                for (int i = 0; i < integers.size(); i++) {
+                    LeiDian.getInstance().removeByIndex(integers.get(i));
+                }
+                LogUtils.e("移除指定的模拟器，size:" + integers.size());
+                LogUtils.e("添加模拟器");
+                return LeiDian.getInstance().add(controller.getEdit_ld_name().getText());
+            }
+        }).map(new Function<String, String>() {
+            @Override
+            public String apply(String s) throws Exception {
+                getSimulatorList();
+                LeiDian.getInstance().modifyDisplay(1, 720, 1080, 240);
+                MobileBrand mobileBrand = MobileBrandUtils.getRandomMobileBrand();
+                LeiDian.getInstance().modifyManufacturer(1, mobileBrand.getBrand());
+                LeiDian.getInstance().modifyModel(1, mobileBrand.getTitle());
+                LeiDian.getInstance().modifyPhoneNumber(1, RandomPhoneNumber.createMobile(1));
+                LeiDian.getInstance().modifyImsi(1, true, 0);
+                LeiDian.getInstance().modifyImei(1, true, 0);
+                LeiDian.getInstance().modifySimSerial(1, true, 0);
+                LeiDian.getInstance().modifyAndroidId(1, true, 0);
+                LeiDian.getInstance().modifyMac(1, true, "");
+                LogUtils.e("修改模拟器配置、安装APK");
+                return LeiDian.getInstance().installApp(1, "C:/Users/Administrator/Desktop/ZSHY无限重启视频.apk");
+            }
+        })
+                .delay(20, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(new DisposableObserver<Object>() {
+                    @Override
+                    public void onNext(Object o) {
+                        LogUtils.e("启动APK");
+                        LeiDian.getInstance().runApp(1, "com.sss.michael");
+                        onComplete();
+                        if (!isDisposed()) {
+                            dispose();
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        getSimulatorList();
+                        LogUtils.e("完毕");
+                    }
+                });
+
     }
 }
